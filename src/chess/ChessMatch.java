d2b139f9e365a3a5c91c65be8c0e665dad04d52e
EnzoCalligaris <enzocontatobr@gmail.com>
2025-08-14 10:52:09 -0300

    Special move - En Passant - PART 2

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -128,6 +128,23 @@ public class ChessMatch {
             rook.increaseMoveCount();
         }
 
+        //#Specialmove en passant
+        if (p instanceof Pawn){
+            if (source.getColumn() != target.getColumn() && capturedPiece == null){
+                Position pawnPosition;
+                if (p.getColor() == Color.WHITE){
+                    pawnPosition = new Position(target.getRow() + 1, target.getColumn());
+                }
+                else {
+                    pawnPosition = new Position(target.getRow() - 1, target.getColumn());
+
+                }
+                capturedPiece = board.removePiece(pawnPosition);
+                capturedPieces.add(capturedPiece);
+                piecesOnTheBoard.remove(capturedPiece);
+            }
+        }
+
         return capturedPiece;
     }
 
@@ -158,6 +175,22 @@ public class ChessMatch {
             ChessPiece rook = (ChessPiece)board.removePiece(targetT);
             board.placePiece(rook, souceT);
             rook.decreseMoveCount();
+        }
+
+        //#Specialmove en passant
+        if (p instanceof Pawn){
+            if (source.getColumn() != target.getColumn() && capturedPiece == enPassantVulnerable){
+                ChessPiece pawn = (ChessPiece)board.removePiece(target);
+                Position pawnPosition;
+                if (p.getColor() == Color.WHITE){
+                    pawnPosition = new Position(3, target.getColumn());
+                }
+                else {
+                    pawnPosition = new Position(4, target.getColumn());
+
+                }
+                board.placePiece(pawn, pawnPosition);
+            }
         }
 
     }
